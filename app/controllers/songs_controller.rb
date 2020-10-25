8dd7a069dd487e093e0f14886ea49637389edc5f
katiethile <lethikatie@gmail.com>
2020-10-24 18:48:25 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,7 @@ class SongsController < Sinatra::Base
     register Sinatra::ActiveRecordExtension
     set :session_secret, "my_application_secret"
     set :views, Proc.new { File.join(root, "../views/") }
+    enable :sessions 
     use Rack::Flash 
 
     get '/songs' do 
@@ -25,12 +26,8 @@ class SongsController < Sinatra::Base
         @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
         @song.genre_ids = params[:genres]
         @song.save
-       # binding.pry
          flash[:message] = "Successfully created song."
-         binding.pry 
          redirect to("/songs/#{@song.slug}")
-        #erb :"/songs/show", locals: {message: "Successfully created song."}
-
       end
 
       get '/songs/:slug/edit' do
@@ -41,14 +38,12 @@ class SongsController < Sinatra::Base
       
       patch '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
-      
-        @song.update(:artist => params[:artist], :genre => params[:genre])
+
+        @song.update(params[:song])
       
         @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
         @song.save
-         flash[:message] = "Successfully created song."
+         flash[:message] = "Successfully updated song."
         redirect to("/songs/#{@song.slug}")
-       # erb :"/songs/show", locals: {message: "Song successfully updated."}
-
       end
 end 
