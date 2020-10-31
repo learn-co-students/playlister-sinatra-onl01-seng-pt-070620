d59d8d801bdc5bd6514e9084cd23a0944aeab1cc
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-10-31 14:20:00 -0700

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,82 @@
+
+require 'sinatra/base'
+require 'rack-flash'
+
+class SongsController < Sinatra::Base
+    enable :sessions
+    use Rack::Flash
+    register Sinatra::ActiveRecordExtension
+    set :session_secret, "my_application_secret"
+    set :views, Proc.new { File.join(root, "../views/") }
+
+    get '/songs' do 
+        @songs = Song.all
+        erb :"songs/index"
+    end 
+
+    get '/songs/new' do
+        @genres = Genre.all
+        erb :"songs/new"
+    end 
+
+    get '/songs/:slug' do 
+        slug = params[:slug]
+        @song = Song.find_by_slug(slug)
+        erb :"songs/show"
+    end 
+
+    post '/songs' do
+        @song = Song.create(:name => params[:song][:name])
+
+        song_artist = params[:song][:artist]
+        if Artist.find_by(:name => song_artist)
+            artist = Artist.find_by(:name => song_artist)
+        else
+            artist = Artist.create(:name => song_artist)
+        end 
+        @song.artist = artist
+        song_genres = params[:song][:genres]
+        song_genres.each do |genre|
+            @song.genres << Genre.find(genre)
+        end 
+        @song.save
+
+        flash[:message] = "Successfully created song."
+        redirect to "songs/#{@song.slug}"
+    end 
+
+    get '/songs/:slug/edit' do 
+        slug = params[:slug]
+        @song = Song.find_by_slug(slug)
+        erb :"songs/edit"
+    end 
+   
+    patch '/songs/:slug' do 
+        song = Song.find_by_slug(params[:slug])
+        song.name = params[:song][:name]
+
+        artist_name = params[:song][:artist]
+        if Artist.find_by(:name => artist_name)
+            if song.artist.name != artist_name
+                song.artist = Artist.find_by(:name => artist_name)
+            end 
+        else 
+            song.artist = Artist.create(:name => artist_name)
+        end 
+
+        if song.genres
+            song.genres.clear
+        end 
+
+        genres = params[:song][:genres]
+        genres.each do |genre|
+            song.genres << Genre.find(genre)
+        end 
+
+        song.save
+        flash[:message] = "Successfully updated song."
+        redirect to "songs/#{song.slug}"
+    end 
+
+
+end 
